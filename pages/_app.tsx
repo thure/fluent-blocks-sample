6ac84879095c7c02d687da8fffc5bddd778c3991
Will Shown <willshown@microsoft.com>
2022-06-09 18:39:48 -0700

    Get Hello World running in Fluent Blocks within Next.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
+import { FluentBlocksProvider } from '@fluent-blocks/react'
+
 const _globals = require('../styles/globals.css')
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FluentBlocksProvider
+      themeName="light"
+      accentScheme="teams"
+      iconSpriteUrl="https://cdn.jsdelivr.net/npm/@fluent-blocks/basic-icons@9.1.2/basic-icons.svg"
+    >
+      <Component {...pageProps} />
+    </FluentBlocksProvider>
+  )
 }
 
 export default MyApp
